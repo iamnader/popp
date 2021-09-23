f98a34ac3625fe309a73d08af00d69e0354836e9
Nader Akhnoukh <iamnader@gmail.com>
2021-09-23 16:02:11 +0100

    another image test

--- components/GetStarted.js
+++ components/GetStarted.js
@@ -1,8 +1,6 @@
-import Image from 'next/image';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import Layout from 'components/Layout';
 import WalletConnectButton from 'components/WalletConnectButton';
 
 const GetStarted = () => {
@@ -17,12 +15,7 @@ const GetStarted = () => {
           Proof of Personhood Passport
         </Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center', my: 4 }}>
-          <img
-            src="./images/passportSmall.png"
-            alt="Personhood Passport"
-            width={128}
-            height={171}
-          />
+          <img src="./images/passportSmall.png" alt="Personhood Passport" />
         </Box>
         <Typography variant="h5" align="center">
           💡 Proof of Personhood Passport (PoPP) is a transportable proof of
